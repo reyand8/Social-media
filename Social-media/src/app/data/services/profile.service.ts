efc5bbe0fee468d989bf0f1efc03e56988f897ce
Andrey <andreeey.gg@gmail.com>
2025-01-08 17:36:31 +0100

    add profile service

---
+++ Social-media/src/app/data/services/profile.service.ts
@@ -0,0 +1,46 @@
+import {inject, Injectable, signal, WritableSignal} from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import {catchError, map, Observable, of, tap} from 'rxjs';
+
+import {Profile} from '../interfaces/profile.interface';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+  http: HttpClient = inject(HttpClient)
+  baseApiUrl: string = 'http://localhost:5001/api/'
+  myProfile: WritableSignal<Profile | null> = signal<Profile | null>(null)
+
+  getProfiles(): Observable<Profile[]> {
+    return this.http.get<Profile[]>(`${this.baseApiUrl}persons`)
+  }
+
+  getPerson(id: string): Observable<Profile> {
+    return this.http.get<Profile>(`${this.baseApiUrl}persons/${id}`)
+  }
+
+  getMyProfile(): Observable<Profile | null> {
+    return this.http.get<Profile>(`${this.baseApiUrl}persons/me`).pipe(
+      tap({
+        next: (res) => {
+          this.myProfile.set(res);
+        },
+      }),
+      catchError(() => {
+        return of(null);
+      })
+    );
+  }
+
+  getFollowing(userId: string): Observable<Profile[]> {
+    return this.http.get<Profile[]>(`${this.baseApiUrl}persons/${userId}/following/`);
+  }
+
+  getFollowingId(userId: string): Observable<number[]> {
+    return this.http.get<Profile[]>(`${this.baseApiUrl}persons/${userId}/following/`).pipe(
+      map((profiles) => profiles.map(profile => profile.id)),
+    );
+  }
+}
