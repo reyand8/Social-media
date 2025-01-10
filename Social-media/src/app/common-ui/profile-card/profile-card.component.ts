03e75b1e771dff70a278e7c849c60c34e7101c7b
Andrey <andreeey.gg@gmail.com>
2025-01-10 17:16:39 +0100

    add profile-card

---
+++ Social-media/src/app/common-ui/profile-card/profile-card.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+
+import {Profile} from '../../data/interfaces/profile.interface';
+import {ImgUrlPipe} from '../../helpers/pipes/img-url.pipe';
+
+
+@Component({
+  selector: 'app-profile-card',
+  templateUrl: './profile-card.component.html',
+  standalone: true,
+  imports: [
+    ImgUrlPipe,
+    AsyncPipe
+  ],
+  styleUrls: ['./profile-card.component.scss']
+})
+export class ProfileCardComponent {
+  @Input() profile!: Profile;
+  @Input() myProfile$!: Observable<Profile | null>;
+  @Input() followingIds!: number[];
+}
