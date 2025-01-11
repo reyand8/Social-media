ba0eb33a43edce66fcd39ae6e905f01e7ca2baf3
Andrey <andreeey.gg@gmail.com>
2025-01-11 19:23:48 +0100

    add search

---
+++ Social-media/src/app/pages/search/search.component.ts
@@ -0,0 +1,49 @@
+import {Component, inject} from '@angular/core';
+import {AsyncPipe, CommonModule} from '@angular/common';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter, Observable, switchMap} from 'rxjs';
+
+import {ProfileCardComponent} from '../../common-ui/profile-card/profile-card.component';
+import {ProfileService} from '../../data/services/profile.service';
+import {Profile} from '../../data/interfaces/profile.interface';
+
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [ProfileCardComponent, AsyncPipe, FormsModule, ReactiveFormsModule, CommonModule],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss'
+})
+export class SearchComponent {
+  profileService: ProfileService = inject(ProfileService);
+  searchForm:  FormGroup<{search: FormControl<string | null>}> =
+    new FormGroup({
+      search: new FormControl(''),
+  });
+
+  filteredProfiles$: Observable<Profile[]>;
+  myProfile$: Observable<null | Profile> = this.profileService.getMyProfile();
+  followingIds: number[] = [];
+
+  constructor() {
+    this.filteredProfiles$ = this.searchForm.controls.search.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      filter((value: string | null) => {
+        return typeof value === 'string' && value.length >= 3;
+      }),
+      switchMap((searchString: string | null) =>
+        searchString ? this.profileService.searchProfiles(searchString) : []
+      )
+    );
+
+    this.myProfile$.subscribe((profile: Profile | null) => {
+      if (profile) {
+        this.profileService.getFollowingId(String(profile.id)).subscribe((ids) => {
+          this.followingIds = ids.map(id => Number(id));
+        });
+      }
+    });
+  }
+}
